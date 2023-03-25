adaa5b0f52f7901d0475f8dad1f8599ef9967ba2
Rajab Mohamed <64719825+RagabMohamedRagab@users.noreply.github.com>
2023-03-25 05:44:04 +0200

    Adding UI OF DataTable.

---
+++ Course.dashboard/Controllers/MVC/AdmistrationController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Course.dashboard.Controllers.MVC {
+    public class AdmistrationController : Controller {
+        [HttpGet]
+        public IActionResult Roles()
+        {
+            return View();
+        }
+    }
+}
