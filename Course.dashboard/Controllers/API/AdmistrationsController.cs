affc266f89e7e7f97422ba55b09509080a9299ee
Rajab Mohamed <64719825+RagabMohamedRagab@users.noreply.github.com>
2023-03-29 15:40:55 +0200

    Create AdmistrationsController API And Endpoint Roles.

---
+++ Course.dashboard/Controllers/API/AdmistrationsController.cs
@@ -0,0 +1,24 @@
+﻿using Course.Service.IServices;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Course.dashboard.Controllers.API {
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class AdmistrationsController : ControllerBase {
+        private readonly IAccountService _accountService;
+
+        public AdmistrationsController(IAccountService accountService)
+        {
+            _accountService = accountService;
+        }
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult Roles()
+        {
+            var data = _accountService.GetRoles().Result;
+            return Ok(new {Data=data,Message="Successfully Request"});
+        }
+    }
+}
