2f5f73c3302f0e35df7792c76cb3ffac49969d8c
Rajab Mohamed <64719825+RagabMohamedRagab@users.noreply.github.com>
2023-04-12 04:45:35 +0200

    Add Social Media Columns in Appuser

--- Course.Domain/Domains/AppUser.cs
+++ Course.Domain/Domains/AppUser.cs
@@ -8,7 +8,16 @@ namespace Course.Domain.Domains {
         }
         [StringLength(maximumLength: 150)]
         public string? About { get; set; }
-        public string? Logo { get; set; } 
+        public string? Logo { get; set; }
+        [Column(TypeName ="nvarchar(150)")]
+        public string? Facebook { get; set; }
+        [Column(TypeName = "nvarchar(150)")]
+        public string? Twitter { get; set; }
+        [Column(TypeName = "nvarchar(150)")]
+        public string? Instagram { get; set; }
+        [Column(TypeName = "nvarchar(150)")]
+        public string? Profile { get; set; } 
+
         public virtual ICollection<UserCourse> UserCourses { get; set; }
     }
 }
---
+++ Course.Repository/Migrations/20230412024052_AddSocialMediaForUser.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Course.Repository.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSocialMediaForUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "32ab264d-bfb3-4819-8400-eeeaa068cfe1");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Facebook",
+                table: "AspNetUsers",
+                type: "nvarchar(150)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Instagram",
+                table: "AspNetUsers",
+                type: "nvarchar(150)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Profile",
+                table: "AspNetUsers",
+                type: "nvarchar(150)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Twitter",
+                table: "AspNetUsers",
+                type: "nvarchar(150)",
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "2c5e174e-3b0e-446f-86af-483d56fd7210",
+                column: "ConcurrencyStamp",
+                value: "a2a19e8f-fa2f-41cb-b262-4ae9e08ffe97");
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "a8279844-8c78-4c00-af6f-f2667706bd22", "be1c42b0-cdf6-4e58-878f-614c072c0b5d", "User", "USER" });
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "8e445865-a24d-4543-a6c6-9443d048cdb9",
+                columns: new[] { "ConcurrencyStamp", "Facebook", "Instagram", "PasswordHash", "Profile", "SecurityStamp", "Twitter" },
+                values: new object[] { "abe60980-1af9-4659-a633-26cc8dc6abcb", null, null, "AQAAAAEAACcQAAAAEFff+WoFo2cRqqgS8LwsFjThUJqNj9cdpeiMa7KQ4LXz3TbcFWEPrHUPO9lrO4q7GQ==", null, "75c82fd3-3504-4a9b-b148-d8c28a091021", null });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "a8279844-8c78-4c00-af6f-f2667706bd22");
+
+            migrationBuilder.DropColumn(
+                name: "Facebook",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Instagram",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Profile",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Twitter",
+                table: "AspNetUsers");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "2c5e174e-3b0e-446f-86af-483d56fd7210",
+                column: "ConcurrencyStamp",
+                value: "1a362f7c-f104-4aef-965f-3e8bc398eaec");
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "32ab264d-bfb3-4819-8400-eeeaa068cfe1", "115a4291-ee19-4a0a-87f1-03a4fc46b5cc", "User", "USER" });
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "8e445865-a24d-4543-a6c6-9443d048cdb9",
+                columns: new[] { "ConcurrencyStamp", "PasswordHash", "SecurityStamp" },
+                values: new object[] { "4bf5d160-8a75-46a9-8be8-888c85966bfc", "AQAAAAEAACcQAAAAEB1S/P9SX5/dcg5HNHfFWwp7Q+oME1kuzpg7SGMoL84qAHDkrkmPL6eNdGsb6Js4Zw==", "f8e631ff-148e-4317-bf3d-90fd0a108e4b" });
+        }
+    }
+}
