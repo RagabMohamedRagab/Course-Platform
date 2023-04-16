3fefa54ce98f0172e1ba519b02f2b912361b1bb9
Rajab Mohamed <64719825+RagabMohamedRagab@users.noreply.github.com>
2023-04-16 22:52:07 +0200

    Finish  From Section Display Data in Profile UI.

--- Course.dashboard/Controllers/MVC/ProfileUserController.cs
+++ Course.dashboard/Controllers/MVC/ProfileUserController.cs
@@ -1,10 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Course.Service.IServices;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Course.dashboard.Controllers.MVC {
     public class ProfileUserController : Controller {
+        private readonly IAccountService _service;
+
+        public ProfileUserController(IAccountService service)
+        {
+            _service = service;
+        }
+        [HttpGet]
         public IActionResult Profile(string email)
         {
-            return View();
+            var result=_service.ProfileUser(email).Result;
+            return View(result);
         }
     }
 }
