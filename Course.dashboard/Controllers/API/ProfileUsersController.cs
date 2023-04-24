4a8399e35abe6507f08a0888e60ee900f73c88ef
Rajab Mohamed <64719825+RagabMohamedRagab@users.noreply.github.com>
2023-04-24 15:04:08 +0200

    Edit in Dashboard.

--- Course.dashboard/Controllers/API/ProfileUsersController.cs
+++ Course.dashboard/Controllers/API/ProfileUsersController.cs
@@ -7,23 +7,60 @@ namespace Course.dashboard.Controllers.API {
     [Route("api/[controller]/[action]")]
     [ApiController]
     public class ProfileUsersController : ControllerBase {
-        private readonly IAccountService _accountService;
+        private readonly IAccountService _service;
 
         public ProfileUsersController(IAccountService accountService)
         {
-            _accountService = accountService;
+            _service = accountService;
         }
 
         [HttpGet]
         [AllowAnonymous]
         public IActionResult Profile(string email)
         {
-            var result=_accountService.ProfileUser(email);
-            if(result == null)
+            var result = _service.ProfileUser(email);
+            if (result == null)
             {
                 return NotFound(new { Data = result, Message = "Your Request Falied" });
             }
             return Ok(new { Data = result, Message = "Done" });
         }
+        [HttpPost]
+        public IActionResult UpdateUserNameAndPhoto(string email, string username, IFormFile userphoto)
+        {
+            if (_service.UpdateUserInfo(userphoto, username, email).Result)
+            {
+
+                return Ok(new { Message = "Done" });
+            }
+
+            return NotFound(new { Message = "Your Request Falied" });
+        }
+        [HttpPost]
+        public IActionResult UpdateUserSocial(string Facebook, string Twitter, string Instagram, string email)
+        {
+
+            if (_service.UpdateUserSocial(Facebook, Twitter, Instagram, email).Result)
+            {
+
+                return Ok(new { Message = "Done" });
+            }
+
+            return NotFound(new { Message = "Your Request Falied" });
+        }
+        [HttpPost]
+        public IActionResult UpdateUserAbout(string about, string email)
+        {
+            if (_service.UpdateUserAbout(about, email).Result)
+            {
+                return Ok(new { Message = "Done" });
+            }
+            return NotFound(new { Message = "Your Request Falied" });
+        }
     }
 }
+
+
+
+
+
