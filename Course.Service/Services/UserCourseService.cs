dfbab5dc041027bc05ef79f71ad0500a6e661344
Rajab Mohamed <64719825+RagabMohamedRagab@users.noreply.github.com>
2023-06-06 20:00:17 +0300

    Start CoursesUser

--- Course.Service/Services/UserCourseService.cs
+++ Course.Service/Services/UserCourseService.cs
@@ -5,12 +5,14 @@ using Course.Repository.IRepositories;
 namespace Course.Service.Services {
     public class UserCourseService : IUserCourseService {
         private readonly IUserCourseRepository _userCourseRepository;
+        private readonly ICourseRepository _courseRepository; 
         private readonly IUnitOfWork _unitOfWork;
 
-        public UserCourseService(IUserCourseRepository userCourseRepository, IUnitOfWork unitOfWork)
+        public UserCourseService(IUserCourseRepository userCourseRepository, IUnitOfWork unitOfWork, ICourseRepository courseRepository)
         {
             _userCourseRepository = userCourseRepository;
             _unitOfWork = unitOfWork;
+            _courseRepository = courseRepository;
         }
 
         public async Task<bool> Add(UserCourse userCourse)
@@ -30,8 +32,20 @@ namespace Course.Service.Services {
             }
         }
 
-        public Task GetAllUserById(string id, int PageNumber, int PageSize)
+        public async Task GetCourseUserByUserId(string Name, int PageNumber, int PageSize)
         {
+            // Test Cas 1 
+            if(string.IsNullOrEmpty(Name))
+                   return null;
+            var user = await _courseRepository.GetUserByName(Name);
+            // Test Case 2
+            if (user is null)
+                return null;
+            var allUserCourse =await _userCourseRepository.GetAll();
+            // Test Case 3
+            if (!allUserCourse.Any())
+                return null;
+            var getUserCourse = allUserCourse.Where(b => b.UserId == user);
             throw new NotImplementedException();
         }
     }
