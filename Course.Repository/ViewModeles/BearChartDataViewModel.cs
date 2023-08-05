9802fa832ea730b6e2aefa03899d0e7df50f2d39
Rajab Mohamed <64719825+RagabMohamedRagab@users.noreply.github.com>
2023-08-06 00:59:50 +0300

    Add UI Of Charts

---
+++ Course.Repository/ViewModeles/BearChartDataViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace Course.Repository.ViewModeles {
+    public class BearChartDataViewModel {
+       public int TotalCount { get;set; }
+        public List<string> ChartMonth { get; set; }
+    }
+}
