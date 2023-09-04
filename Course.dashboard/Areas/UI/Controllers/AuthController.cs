d3ef28d35ec10948762664fb1b5cb7cfee91b080
Rajab Mohamed <64719825+RagabMohamedRagab@users.noreply.github.com>
2023-09-04 22:08:31 +0300

    End From Register UI and Login Ui and Contact UI .

---
+++ Course.dashboard/Areas/UI/Controllers/AuthController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Course.dashboard.Areas.UI.Controllers {
+	[Area("UI")]
+	public class AuthController : Controller {
+		public IActionResult Register()
+		{
+			return View();
+		}
+		public IActionResult Login()
+		{
+			return View();
+		}
+		public IActionResult ContactUs()
+		{
+			return View();
+		}
+	}
+}
