7b9d92dd259d2cde99b1d2bb56aa91bf717d9698
Rajab Mohamed <64719825+RagabMohamedRagab@users.noreply.github.com>
2023-09-07 22:52:02 +0300

    Add Gallery In Database

---
+++ Course.Domain/Domains/Gallery.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Course.Domain.Domains {
+    public class Gallery {
+        public int Id { get; set; }
+        [DataType(DataType.Text), MaxLength(150)]
+        public string? Text { get; set; }
+        [MaxLength(150), DataType(DataType.ImageUrl)]
+        public string? Img { get; set; }
+    }
+}
---
+++ Course.Repository/Migrations/20230907194604_AddGalleryInTable.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Course.Repository.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddGalleryInTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Galleries",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Text = table.Column<string>(type: "nvarchar(150)", maxLength: 150, nullable: true),
+                    Img = table.Column<string>(type: "nvarchar(150)", maxLength: 150, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Galleries", x => x.Id);
+                });
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "2c5e174e-3b0e-446f-86af-483d56fd7210",
+                column: "ConcurrencyStamp",
+                value: "b4f76633-3e39-4149-9d4a-76b00b748e77");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "8e445865-a24d-4543-a6c6-9443d048cdb9",
+                columns: new[] { "ConcurrencyStamp", "PasswordHash", "SecurityStamp" },
+                values: new object[] { "30977d56-ffcc-44f4-b62f-8763acdb2af3", "AQAAAAEAACcQAAAAEOfrngFYX143tlI5VWbgmJ7GONcPOYHnn+s/3mfGRVVTp7uX6j7mqd/msb82Gt442w==", "cd3970a0-7aa8-45a3-a3aa-58eaa9384046" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Galleries");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "2c5e174e-3b0e-446f-86af-483d56fd7210",
+                column: "ConcurrencyStamp",
+                value: "ac5537be-8a9c-48fd-93d0-3ecb825480e1");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "8e445865-a24d-4543-a6c6-9443d048cdb9",
+                columns: new[] { "ConcurrencyStamp", "PasswordHash", "SecurityStamp" },
+                values: new object[] { "ad85d980-06e9-4f20-990d-a3411bb175e4", "AQAAAAEAACcQAAAAEN0iSmTe6SWOnkqbIzxPfT/vWNlOzVruO/pmN3JVt53LfNQN5NWQRZRSYHuWMiJlIw==", "da6f9075-3c26-4e7b-86eb-23d537cb1534" });
+        }
+    }
+}
