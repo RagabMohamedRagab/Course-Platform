5c20b1babb00b57ebbaed3b2c78c5f67f6d3b242
Rajab Mohamed <64719825+RagabMohamedRagab@users.noreply.github.com>
2023-10-10 00:29:19 +0300

    Alter In Something.

--- Course.dashboard/Areas/UI/GateWay/Models/StripeRedirection.cs
+++ Course.dashboard/Areas/UI/GateWay/Models/StripeRedirection.cs
@@ -5,8 +5,8 @@ using Microsoft.AspNetCore.Http;
 namespace Course.dashboard.Areas.UI.GateWay.Models
 {
     public class StripeRedirection {
-        public static string Currency = "EGP";
+        public static string Currency = "USD";
         public static string SuccessUrl = "https://localhost:7047/UI/Home/Index";
-        public static string CancelUrl = "https://localhost:7047/UI/Home/Indexl";
+        public static string CancelUrl = "https://localhost:7047/UI/Home/AboutUs";
     }
 }
